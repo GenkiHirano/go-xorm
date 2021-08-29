058aa1600798a90cdf340f02a7f3a1e29134131f
EikoNakashima <68436861+EikoNakashima@users.noreply.github.com>
2021-08-29 15:57:32 +0900

    test

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/EikoNakashima/go-xorm.git
 
 go 1.15
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0
+	xorm.io/xorm v1.2.3
+)
--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"time"
 
+	_ "github.com/go-sql-driver/mysql"
+	"xorm.io/xorm"
+)
+
+type User struct {
+	Id      int64
+	Name    string
+	Salt    string
+	Age     int
+	Passwd  string    `xorm:"varchar(200)"`
+	Created time.Time `xorm:"created"`
+	Updated time.Time `xorm:"updated"`
+}
+
+func main() {
+	engine, err := xorm.NewEngine("mysql", "root:root@tcp([127.0.0.1]:3306)/sample_db?charset=utf8mb4&parseTime=true")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = engine.Sync2(new(User))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var user User
+	user.Name = "名前"
+	user.Passwd = "パスワード"
+	engine.Table("user").Insert(user)
+
+	fmt.Println("Insert終了しました。")
 }
