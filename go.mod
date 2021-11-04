d0aa263d86740f55842d0dc8495598bbf8dcc4cb
GenkiHirano <g.hirano@gemcook.com>
2021-11-04 12:41:00 +0900

    refactor: go-xorm

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/go-sql-driver/mysql v1.6.0
+	github.com/joho/godotenv v1.4.0
 	xorm.io/xorm v1.2.3
 )
--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"log"
-	"time"
+	"os"
+	"strconv"
+	"strings"
 
+	"github.com/go-sql-driver/mysql"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 	"xorm.io/xorm"
 )
 
@@ -13,16 +17,23 @@ type User struct {
 	Id       int64 `xorm:"id"`
 	Name     string
 	Age      int
-	Password string    `xorm:"password"`
-	Created  time.Time `xorm:"created"`
-	Updated  time.Time `xorm:"updated"`
+	Password string `xorm:"password"`
 }
 
 func main() {
-	engine, err := xorm.NewEngine("mysql", "root:root@tcp([127.0.0.1]:3306)/sample_db?charset=utf8mb4&parseTime=true")
-	if err != nil {
-		log.Fatal(err)
+
+	err := godotenv.Load(".env")
+	addr := getAddr(os.Getenv("DATABASE_WRITE_HOST"), os.Getenv("DATABASE_READ_HOST"))
+
+	conf := &mysql.Config{
+		Net:                  "tcp",
+		Addr:                 addr,
+		DBName:               os.Getenv("DATABASE_NAME"),
+		User:                 os.Getenv("MAINTENANCE_DATABASE_USER"),
+		Passwd:               os.Getenv("MAINTENANCE_DATABASE_PASSWORD"),
+		AllowNativePasswords: true,
 	}
+	engine, err := xorm.NewEngine("mysql", conf.FormatDSN())
 
 	err = engine.Sync2(new(User))
 	if err != nil {
@@ -32,31 +43,33 @@ func main() {
 	// メソッドを実行します
 	// Insert(*engine)
 	// Get(*engine)
-	// Find(*engine)
+	Find(*engine)
 	// Count(*engine)
 	// Update(*engine)
 	// Delete(*engine)
-	// Table(*engine)
-	// Select(*engine)
-	// SetID(*engine)
-	// Sum(*engine)
-	// SumsInt(*engine)
-	// WhereAnd(*engine)
-	// WhereOr(*engine)
-	// OrderBy(*engine)
-	// Asc(*engine)
-	// Desc(*engine)
-	SQL(*engine)
-	Query(*engine)
+}
 
+func getAddr(host, port string) string {
+	var addr string
+	if strings.Contains(host, ":") {
+		addr = host
+	} else {
+		pInt := 3306
+		if p, err := strconv.Atoi(port); err == nil {
+			pInt = p
+		}
+		addr = fmt.Sprintf("%v:%v", host, pInt)
+	}
+	return addr
 }
 
 // Insert
 func Insert(engine xorm.Engine) {
 	user := User{
+		Id:       1,
 		Name:     "太郎",
-		Password: "パスワード",
 		Age:      20,
+		Password: "パスワード",
 	}
 	_, err := engine.Table("user").Insert(user)
 	if err != nil {
@@ -72,10 +85,10 @@ func Get(engine xorm.Engine) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("user:", user)
 	if !result {
 		log.Fatal("Not Found")
 	}
+	fmt.Println("user:", user)
 }
 
 // Find 複数取得(複数レコードを取得)
@@ -102,6 +115,7 @@ func Count(engine xorm.Engine) {
 // Update
 func Update(engine xorm.Engine) {
 	user := User{
+		Id:       1,
 		Name:     "更新した名前",
 		Password: "更新したパスワード",
 		Age:      30,
@@ -128,106 +142,4 @@ func Delete(engine xorm.Engine) {
 	}
 	fmt.Println("user:", user)
 	fmt.Println(result)
-}
-
-func Table(engine xorm.Engine) {
-	user := User{}
-	// ここでテーブルをユーザーテーブルを指定します。
-	session := engine.Table("user")
-	_, err := session.Where("id = ?", 1).Get(&user)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("user:", user)
-}
-
-func Select(engine xorm.Engine) {
-	user := User{}
-	// ここでテーブルをユーザーテーブルを指定します。
-	session := engine.Table("user")
-	result, err := session.Select("name").Where("id = ?", 1).Get(&user)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("user:", user)
-	fmt.Println("result:", result)
-}
-
-func Sum(engine xorm.Engine) {
-	user := User{}
-	sumFloat64, _ := engine.Sum(user, "id")
-	print(" sumFloat64: ", sumFloat64)
-}
-
-func SumsInt(engine xorm.Engine) {
-	user := User{}
-	sumInt, _ := engine.SumInt(user, "id")
-	print(" sumInt: ", sumInt)
-}
-
-func WhereAnd(engine xorm.Engine) {
-	users := []User{}
-	session := engine.Table("user")
-	err := session.Where("id > ?", 1).And("age = ?", 30).Find(&users)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("whereAndUsers:", users)
-}
-
-func WhereOr(engine xorm.Engine) {
-	users := []User{}
-	session := engine.Table("user")
-	err := session.Where("id > ?", 1).Or("age = ?", 30).Find(&users)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("whereOrUsers:", users)
-}
-
-func OrderBy(engine xorm.Engine) {
-	users := []User{}
-	session := engine.Table("user")
-	err := session.OrderBy("id desc").Find(&users)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("users:", users)
-}
-
-func Asc(engine xorm.Engine) {
-	users := []User{}
-	session := engine.Table("user")
-	err := session.Asc("id").Find(&users)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("AscUsers:", users)
-}
-
-func Desc(engine xorm.Engine) {
-	users := []User{}
-	session := engine.Table("user")
-	err := session.Desc("id").Find(&users)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("DescUsers:", users)
-}
-
-func SQL(engine xorm.Engine) {
-	users := []User{}
-	err := engine.SQL("select * from user").Find(&users)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("users:", users)
-}
-
-func Query(engine xorm.Engine) {
-	results, err := engine.Query("select * from user")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("results:", results)
 }
